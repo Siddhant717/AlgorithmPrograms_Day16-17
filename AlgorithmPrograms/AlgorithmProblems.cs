622f4dc82b4c5000791b18fc390d077cda1c2378
Siddhant <santoshshrivastava39@gmail.com>
2022-07-02 00:24:19 +0530

    Merge Sorting

--- AlgorithmPrograms/AlgorithmProblems.cs
+++ AlgorithmPrograms/AlgorithmProblems.cs
@@ -8,35 +8,73 @@ namespace AlgorithmPrograms
 {
     public class AlgorithmProblems
     {
-        public static void BubbleSorting(int[] arr)
-        {
-            int temp;
-            for (int j = 0; j <= arr.Length - 2; j++)
-            {
-                for (int i = 0; i <= arr.Length - 2; i++)
-                {
-                    if (arr[i] > arr[i + 1])
-                    {
-                        temp = arr[i + 1];
-                        arr[i + 1] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
-            }
-            Console.WriteLine("Sorted Array is :");
+		public void merge(int[] arr, int l, int m, int r)
+		{
+			int n1 = m - l + 1;
+			int n2 = r - m;
 
-            foreach (int p in arr)
-            {
-                Console.Write(p + " ");
-            }
-        }
-        public static void printArray(int[] arr)
-        {
-            int n = arr.Length;
-            for (int i = 0; i < n; ++i)
-                Console.Write(arr[i] + " ");
-            Console.WriteLine();
-        }
+			int[] L = new int[n1];
+			int[] R = new int[n2];
 
-    }
+			for (int f = 0; f < n1; ++f)
+				L[f] = arr[l + f];
+			for (int d = 0; d < n2; ++d)
+				R[d] = arr[m + 1 + d];
+
+
+
+			int i = 0, j = 0;
+
+			int k = l;
+			while (i < n1 && j < n2)
+			{
+				if (L[i] <= R[j])
+				{
+					arr[k] = L[i];
+					i++;
+				}
+				else
+				{
+					arr[k] = R[j];
+					j++;
+				}
+				k++;
+			}
+
+			while (i < n1)
+			{
+				arr[k] = L[i];
+				i++;
+				k++;
+			}
+
+			while (j < n2)
+			{
+				arr[k] = R[j];
+				j++;
+				k++;
+			}
+		}
+
+		public void sort(int[] arr, int l, int r)        
+		{
+			if (l < r)                                     
+			{
+				int m = (l + r) / 2;                     
+
+				sort(arr, l, m);                        
+				sort(arr, m + 1, r);
+
+				merge(arr, l, m, r);
+			}
+		}
+
+		public static void printArray(int[] arr)
+		{
+			int n = arr.Length;                             
+			for (int i = 0; i < n; ++i)                
+				Console.Write(arr[i] + " ");         
+			Console.WriteLine();
+		}
+	}
 }
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -2,14 +2,20 @@
 
 namespace AlgorithmPrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 22, 45, 10, 50, 16 };
-            Console.WriteLine("Given Array is ");
-            AlgorithmProblems.printArray(arr);
-           AlgorithmProblems.BubbleSorting(arr);
+            int[] array = { 12, 11, 13, 5, 6, 7 };
+
+            Console.WriteLine("Given Array");
+            AlgorithmProblems.printArray(array);
+
+            AlgorithmProblems ob = new AlgorithmProblems();
+            ob.sort(array, 0, array.Length - 1);
+
+            Console.WriteLine("Sorted array");
+            AlgorithmProblems.printArray(array);
         }
     }
 }
